e4a3f689411c41979f87deb1a59b95a1825e08f8
Manuel Quiñones <manuel.por.aca@gmail.com>
2022-09-29 16:17:19 -0300

    Apply code formatting
    Also add a newline to JSON files in the script, to adapt to the
    pre-commit hook.
    
    https://phabricator.endlessm.com/T33897

--- tools/init2json.py
+++ tools/init2json.py
@@ -10,34 +10,34 @@ from collections import OrderedDict
 
 def parse_channel_from_config(config, id):
     channel = {
-        'id': id,
-        'include_node_ids': [],
-        'exclude_node_ids': [],
+        "id": id,
+        "include_node_ids": [],
+        "exclude_node_ids": [],
     }
 
-    section = f'kolibri-{id}'
+    section = f"kolibri-{id}"
     if section not in config.sections():
         return channel
 
     channel_section = config[section]
-    include_node_ids = channel_section.get('include_node_ids', '').split()
-    exclude_node_ids = channel_section.get('exclude_node_ids', '').split()
+    include_node_ids = channel_section.get("include_node_ids", "").split()
+    exclude_node_ids = channel_section.get("exclude_node_ids", "").split()
 
-    channel['include_node_ids'] = include_node_ids
-    channel['exclude_node_ids'] = exclude_node_ids
+    channel["include_node_ids"] = include_node_ids
+    channel["exclude_node_ids"] = exclude_node_ids
 
     # FIXME setting channel version to 0 for now, so Kolibri ContentManifest validates
-    channel['version'] = 0
+    channel["version"] = 0
 
     return channel
 
 
 def parse_metadata_from_section(metadata_section):
     return OrderedDict(
-        title = metadata_section.get('title'),
-        subtitle = metadata_section.get('subtitle'),
-        description = metadata_section.get('description'),
-        required_gigabytes = int(metadata_section.get('required_gigabytes'))
+        title=metadata_section.get("title"),
+        subtitle=metadata_section.get("subtitle"),
+        description=metadata_section.get("description"),
+        required_gigabytes=int(metadata_section.get("required_gigabytes")),
     )
 
 
@@ -46,35 +46,47 @@ def _get_channels_list_hash(channels_list):
     return hashlib.md5(json.dumps(channels_list, sort_keys=True).encode()).hexdigest()
 
 
-def ini2json(f, output='collections'):
+def ini2json(f, output="collections"):
     config = configparser.ConfigParser()
     config.read(f)
 
-    out = {'channels': []}
+    out = {"channels": []}
 
-    channel_ids = config['kolibri']['install_channels']
+    channel_ids = config["kolibri"]["install_channels"]
     for id in channel_ids.split():
         channel = parse_channel_from_config(config, id)
-        out['channels'].append(channel)
+        out["channels"].append(channel)
 
-    out['channel_list_hash'] = _get_channels_list_hash(out['channels'])
+    out["channel_list_hash"] = _get_channels_list_hash(out["channels"])
 
-    if 'metadata' in config:
-        out['metadata'] = parse_metadata_from_section(config['metadata'])
+    if "metadata" in config:
+        out["metadata"] = parse_metadata_from_section(config["metadata"])
 
     filename, _ext = os.path.splitext(os.path.basename(f))
-    output_path = os.path.join(output, f'{filename}.json')
-    with open(output_path, 'w') as output_file:
+    output_path = os.path.join(output, f"{filename}.json")
+    with open(output_path, "w") as output_file:
         json.dump(out, output_file, indent=2)
+        output_file.write("\n")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description='Convert collection .ini files to manifest.json')
-    parser.add_argument('files', metavar='INI_FILE', type=str, nargs='+',
-                        help='A .ini file to convert to manifest.json')
-    parser.add_argument('-o', '--output', type=str, default='collections',
-                        help='The output directory, collections by default')
+        description="Convert collection .ini files to manifest.json"
+    )
+    parser.add_argument(
+        "files",
+        metavar="INI_FILE",
+        type=str,
+        nargs="+",
+        help="A .ini file to convert to manifest.json",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        default="collections",
+        help="The output directory, collections by default",
+    )
 
     args = parser.parse_args()
     for f in args.files:
